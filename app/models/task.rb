b881a39f301796bd3c5795bf5aa67fa627b93fe7
Stella Miranda <stellamariam@gmail.com>
2021-02-27 19:30:35 -0800

    Create users & Tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,8 @@
 class Task < ApplicationRecord
   #validations
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :description, length: { maximum: 80}
   #relations
   #before_save
   #after_update 
