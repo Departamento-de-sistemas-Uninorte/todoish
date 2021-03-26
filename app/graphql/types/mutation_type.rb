09537fbf08c2e63afba8db1e544043ca71d96b64
Stella Miranda <stellamariam@gmail.com>
2021-03-26 08:27:53 -0700

    GraphQL: Implementing create task mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,8 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
+
+    field :create_task, mutation: Mutations::CreateTask do
+      "This allows to create a Task via GraphQL"
     end
   end
 end
