126743a0582df18f798fac25fb6c358430fc70d6
Stella Miranda <stellamariam@gmail.com>
2021-04-09 09:02:51 -0700

    Implementing Rspec

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  describe "Validations" do
+    # what the app should do
+  end
+end
+
+RSpec.describe Task, type: :model do
+  pending "Peding example"
+end
