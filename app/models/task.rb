c8e63b0a8ea83806f985bb98480c31aa447d931a
Stella Miranda <stellamariam@gmail.com>
2021-04-30 08:05:26 -0700

    Configure active storage and attach image to task in webapp

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,7 @@ class Task < ApplicationRecord
   validates :description, length: { maximum: 80}
   #relations
   belongs_to :user
+  has_one_attached :image, dependent: :destroy
   #before_save
   #after_update 
 end
